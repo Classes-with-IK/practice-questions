6f9fccfef166297f087039f3814439911d9d0361
Uche Emmanuel <ucheemmanuel20222@gmail.com>
2026-08-12 20:07:58 +0100

    Added the Fix components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import AddThree from './components/AddThree.jsx'
-
+import Fix from './components/Fix.jsx'
 
 function App() {
 
   return (
     <>
-     <AddThree />
+      <AddThree />
+      <Fix />
     </>
   )
 }

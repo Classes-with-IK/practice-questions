848f83d2aec459c85bfe26f840b9e36ccb3fbedb
Uche Emmanuel <ucheemmanuel20222@gmail.com>
2026-08-13 10:04:45 +0100

    Added FixNumber components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import AddThree from './components/AddThree.jsx'
 import Fix from './components/Fix.jsx'
-
+import FixNumber from './components/FixNumber.jsx'
 function App() {
 
   return (
     <>
       <AddThree />
       <Fix />
+      <FixNumber />
     </>
   )
 }

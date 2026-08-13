e66281b4cfd9b42f895bd4b7b6e0bf2b6b561593
Uche Emmanuel <ucheemmanuel20222@gmail.com>
2026-08-13 10:20:21 +0100

    Added the Repeat component changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import AddThree from './components/AddThree.jsx'
 import Fix from './components/Fix.jsx'
 import FixNumber from './components/FixNumber.jsx'
 import Stop from './components/Stop.jsx'
-
+import Repeat from './components/Repeat.jsx'
 
 
 
@@ -15,6 +15,7 @@ function App() {
       <Fix />
       <FixNumber />
       <Stop />
+      <Repeat />
     </>
   )
 }
---
+++ src/components/Repeat.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function Repeat() {
+  const [number, setNumber] = useState(0);
+
+  function increaseNumber() {
+    setNumber(number + 1);
+  }
+
+  function decreaseNumber() {
+    if (number <= 0) {
+      return;
+    }
+
+    setNumber(number - 1);
+  }
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="flex flex-row gap-12">
+        <button
+          onClick={() => decreaseNumber()}
+          className="size-12 bg-red-600 text-2xl text-white">
+          -
+        </button>
+
+        <p>{number}</p>
+
+        <button
+          onClick={() => increaseNumber()}
+          className="size-12 bg-green-600 text-2xl text-white">
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Repeat;
